11bb9afee0c477590ad0520bc2c85a415a837663
Oskar Yildiz <yildizoskar@gmail.com>
2020-10-12 20:28:23 +0200

    added updated algorithm

--- multigraph/algorithms.go
+++ multigraph/algorithms.go
@@ -6,8 +6,8 @@ import (
 )
 
 func (g *Graph) DiffuseInformation(seed []int64, beta float64, diffusionCase string) map[int64]time.Time {
-	g.lock.Lock()
-	defer g.lock.Unlock()
+	g.lock.RLock()
+	defer g.lock.RUnlock()
 	informedNodes := make(map[int64]time.Time)
 	changed := true
 
@@ -15,9 +15,13 @@ func (g *Graph) DiffuseInformation(seed []int64, beta float64, diffusionCase str
 		informedNodes[nodeID] = time.Time{}
 	}
 
+	// fmt.Println(informedNodes)
+
 	for changed {
 		lines := g.AdjacentEdges(informedNodes)
-		fmt.Println(lines)
+		// fmt.Println(lines)
+		// fmt.Println(lines)
+		// fmt.Println(lines)
 		for i := range lines {
 			u := lines[i].From()
 			v := lines[i].To()
@@ -27,10 +31,42 @@ func (g *Graph) DiffuseInformation(seed []int64, beta float64, diffusionCase str
 
 			if informedNodes[u.ID()].IsZero() || informedNodes[u.ID()].Before(dt) {
 				if nodeV, exists := informedNodes[v.ID()]; !exists || (dt.Before(nodeV) && !nodeV.IsZero()) {
-					fmt.Println(exists, dt)
-					fmt.Println(v.ID())
+					// fmt.Println(exists, dt)
+					// fmt.Println(v.ID())
 					informedNodes[v.ID()] = dt
 					changed = true
+				}
+			}
+		}
+	}
+	return informedNodes
+}
+
+func (g *Graph) DiffuseInformationSimple(seed []int64, beta float64, diffusionCase string) map[int64]int64 {
+	g.lock.RLock()
+	defer g.lock.RUnlock()
+	informedNodes := make(map[int64]int64)
+	for _, nodeID := range seed {
+		informedNodes[nodeID] = 0
+	}
+	needsUpdate := true
+
+	for needsUpdate {
+		needsUpdate = false
+		lines := g.AdjacentEdgesSimple(informedNodes)
+		fmt.Println("iteration")
+		for i := range lines {
+			v := lines[i].To()
+			dn := lines[i].DiffusionNumber()
+
+			if _, exists := informedNodes[v.ID()]; !exists {
+				informedNodes[v.ID()] = dn
+				fmt.Println(informedNodes)
+				needsUpdate = true
+			} else {
+				if _, exists := informedNodes[v.ID()]; exists && dn < informedNodes[v.ID()] {
+					informedNodes[v.ID()] = dn
+					needsUpdate = true
 				}
 			}
 		}
